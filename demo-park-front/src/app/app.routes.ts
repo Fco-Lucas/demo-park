648be9d1e1b8748e53940cb691428141e28eb18c
Francisco Lucas <128393859+Fco-Lucas@users.noreply.github.com>
2025-02-23 14:08:08 -0300

    Delete demo-park-front directory

--- demo-park-front/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { authRoutes } from './pages/auth/auth.routes';
-
-export const routes: Routes = [
-  ...authRoutes,
-  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Rota padrão redireciona para o login
-  { path: '**', redirectTo: '/login' }, // Rota curinga redireciona para o login
-];
